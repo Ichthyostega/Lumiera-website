440ee7bbf08364bcd39736d70986db434ba8f919
Cillian de Roiste <goibhniu@lumiera.(none)>
2009-01-10 19:16:15 +0000

    set up the credits page properly, with it's own asciidoc conf to tidy up the html

---
+++ js/scroller.js
@@ -0,0 +1,10 @@
+function pageScrollRec(current_position) {
+    window.scrollBy(0, 1); // horizontal and vertical scroll increment
+    if (current_position < window.pageYOffset) {
+      setTimeout('pageScrollRec(window.pageYOffset)', 50); // scrolls every 100 milliseconds
+    }
+}
+
+function pageScroll() {
+    setTimeout('pageScrollRec(0)', 3000);
+}
