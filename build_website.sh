2a09bca8d049a5cee7e924484959a4fd2e738dd9
Christian Thaeter <ct@pipapo.org>
2009-08-25 20:02:51 +0200

    new website build script

---
+++ build_website.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DEFAULT_CONF=layout1
+
+
+# for every .txt file
+find -name '*.txt' |
+	while read file; do
+		echo $file
+		# when the .txt is newer than an existing .html
+		if [[ "$file" -nt "${file%*.txt}.html" ]]; then
+			# use the default config file
+			conf="${DEFAULT_CONF}.conf"
+			# or if there is a .conf file with the same basename as the .txt file use that instead
+			if [[ -e "${file%*.txt}.conf" ]]; then
+				conf="${file%*.txt}.conf"
+			fi
+			# run asciidoc over it
+			python /usr/bin/asciidoc --unsafe --backend=xhtml11 \
+				--attribute icons --attribute \
+				iconsdir=./images/icons --attribute=badges \
+				--attribute=revision="$VERS"  --attribute=date="$DATE" \
+				--conf-file="${conf}" \
+				"$file"
+		fi
+
+
+	done
+
