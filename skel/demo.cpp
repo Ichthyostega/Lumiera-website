a13ea6c6bea5effd192bdce47f2351f7944333bf
Ichthyostega <prg@ichthyostega.de>
2025-07-12 16:54:15 +0200

    Research:Demo-Skeleton: add minimal CMake build and C++ Hello-World
    - use a hierarchical CMake build
    - use a subdirectory and a self-contained CMake definition for each individual demo

---
+++ skel/demo.cpp
@@ -0,0 +1,21 @@
+/*
+  demo.cpp  -  simple GTK Application
+
+   Copyright (C)
+     2025,            Benny Lyons <benny.lyons@gmx.net>
+     2025,            Hermann Vosseler <Ichthyostega@web.de>
+
+  This program is free software;
+  TODO License
+
+* *****************************************************************/
+
+
+#include <iostream>
+
+int
+main (int argc, const char* argv[])
+{
+    std::cout << "Hello Video" << std::endl;
+}
+
